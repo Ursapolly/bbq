b98365c44e14c3037a2e6dbe90b315077e49c781
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-06 14:30:38 +0700

    Alpha version ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_avatar(user)
+    # TODO: upload avatar option
+    asset_path('user2.png')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z\d\-_.]+@[a-zA-Z\d\-_.]+\z/
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Гражданин(ка)#{rand(777)}" if self.name.blank?
+  end
 end
