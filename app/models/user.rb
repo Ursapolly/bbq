78fbd99e1d8b0684f2c7503ea0f1de6df190b99e
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-08 05:57:24 +0700

    Comment model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :comments
 
   validates :name, presence: true, length: {maximum: 35}
 end
