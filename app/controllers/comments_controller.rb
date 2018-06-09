6e6ebc19b2da2d13a4af2689a47b719159390560
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-09 15:19:12 +0700

    Email notifications about new subscriptions & comments with action mailer & sendgrid

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
   before_action :set_event, only: %i[create destroy]
   before_action :set_comment, only: [:destroy]
+
   def create
     @new_comment = @event.comments.build(comment_params)
     @new_comment.user = current_user
     if @new_comment.save
+      notify_subscribers(@event, @new_comment)
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
       render 'events/show', alert: I18n.t('controllers.comments.error')
@@ -33,5 +35,13 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body, :user_name)
+  end
+
+  def notify_subscribers(event, comment)
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    # по-хорошему, выносим такие методы в background задачи!!!
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
+    end
   end
 end
